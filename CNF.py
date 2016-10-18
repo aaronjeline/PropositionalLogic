76de33eea77951e071448a28412f9b000e7ce89b
aaron <aaron@aaron-pc.home>
2016-10-17 22:34:01 -0400

    Progress, finished forwardchaining

--- CNF.py
+++ CNF.py
@@ -1,3 +1,4 @@
+from enum import Enum
 class Literal:
     symbol = None
     sign = None
@@ -7,4 +8,24 @@ class Literal:
 
     def negate(self):
         return Literal(self.symbol, not self.sign)
+
+class Clause:
+    contents = None
+    def __init__(self, contents):
+        self.contents = contents
+
+class operators(Enum):
+    AND = 0
+    OR = 1
+    NOT = 2
+    IMPLIES = 3
+    EQUALS = 4
+
+def toCNF(phrase):
+    #Reduce all sub-clauses to CNF
+    clauses = filter(lambda x: isinstance(x, Clause), phrase)
+    clauses = list(map(lambda x:toCNF(x), clauses))
+    for i in phrase:
+        if isinstance(i, Clause):
+            phrase[phrase.index(i)] = clauses.pop(0)
 
--- ForwardChaining.py
+++ ForwardChaining.py
@@ -1,14 +1,28 @@
+class Agenda:
+    items = None
+    def __init__(self, items, key=lambda x:x):
+        self.items = list(map(key, items))
+    def __iter__(self):
+        return self
+    def __next__(self):
+        try:
+            return self.items.pop()
+        except IndexError:
+            raise StopIteration
+    def add(self, item):
+        self.items.append(item)
+
+
 def forwardChaining(KB, q):
     count = {}
     inferred = {}
-    agenda = list(filter(lambda x: len(x[0])==0,KB))
+    agenda = Agenda(filter(lambda x: len(x[0])==0,KB), key=lambda x:x[1])
     for i in KB:
         count[i] = len(i[0])
         for j in i[0]:
             inferred[j] = False
         inferred[i[1]] = False
     for i in agenda:
-        agenda.remove(i)
         if i == q:
             return True
         if not inferred[i]:
@@ -16,6 +30,11 @@ def forwardChaining(KB, q):
             for j in filter(lambda x: i in x[0], KB):
                 count[j] -= 1
                 if count[j] == 0:
-                    agenda.append(j[1])
+                    agenda.add(j[1])
     return False
 
+skb = [(tuple(),'a'),(tuple(),'b'),(('a','b'),'m'),(('a','l'),'m'),
+        (('m','l'), 'p'), (('p',),'q')
+       ]
+okb = [(('p',),'q'), (('a','l'),'p'),(('b','m'),'l'),(('a','p'),'m'),(('a','b'),'m'),(tuple(),'a'),(tuple(),'b')]
+print(forwardChaining(okb, 'q'))
